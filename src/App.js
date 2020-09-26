ab1ca1c9014e8a92f051cd2f8299877732249f02
Sofia <sgandsteph@gmail.com>
2020-09-26 20:24:45 +0200

    updates app with readable react html

--- src/App.js
+++ src/App.js
@@ -3,20 +3,14 @@ import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel"
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
-  ]);
+  return (
+    <div>
+      <h1 id="something-special">Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Terrier" />
+      <Pet name="Pepper" animal="Bird" breed="Coca" />
+      <Pet name="Mipsy" animal="Cat" breed="Maincoon" />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, React.createElement, document.getElementById("root"));
